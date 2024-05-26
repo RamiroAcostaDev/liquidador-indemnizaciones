6c8a1dbb604ae4917ae00a9e007839c5ceeda6aa
RamiroAcostaDev <ramiroacosta.dev@gmail.com>
2024-05-26 19:21:25 -0300

    develop update

--- src/app/components/Art1Ley25323.tsx
+++ src/app/components/Art1Ley25323.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useContext } from "react";
 import { ContextProvider } from "../context/ContextProvider";
 import formatearNumeroAmoneda from "../helpers/FormatNumber";
+
 const Art1Ley25323 = () => {
   const context = useContext(ContextProvider);
 
